4216e9fadea1522a31ec358ff2ef13c449f8c81e
dev-hamster <hamsterwave0925@gmail.com>
2024-11-05 00:41:19 +0900

    feat: svg 컴포넌트 사용을 위한 vite 설정

--- week01/src/types/global.d.ts
+++ week01/src/types/global.d.ts
@@ -2,3 +2,8 @@ declare module '*.scss' {
   const content: { [className: string]: string };
   export = content;
 }
+
+declare module '*.svg' {
+  const content: React.FC<React.SVGProps<SVGElement>>;
+  export default content;
+}
