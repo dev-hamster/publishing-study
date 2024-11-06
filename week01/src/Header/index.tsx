7eb501af02c60b8525af9825cd53df661dfa71c1
dev-hamster <hamsterwave0925@gmail.com>
2024-11-06 14:34:59 +0900

    feat: Header 반응형 디자인

---
+++ week01/src/Header/index.tsx
@@ -0,0 +1,58 @@
+import IcoAlarm from '../assets/header/ico_alarm.svg?react';
+import IcoMenu from '../assets/header/ico_menu.svg?react';
+import logo from '../assets/header/logo.png';
+import profilePic from '../assets/header/profilePic.png';
+import headerStyles from './header.module.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <div className={headerStyles.container}>
+        <div className={headerStyles.logo}>
+          <div className={headerStyles.icon}>
+            <img className={headerStyles.iconImage} src={logo} alt='logo' />
+          </div>
+          <div className={headerStyles.titleWrapper}>
+            <h1 className={headerStyles.title}>NFT logoplace</h1>
+          </div>
+        </div>
+        <div className={headerStyles.menu}>
+          <div className={headerStyles.menuIcon}>
+            <IcoMenu />
+          </div>
+
+          <ul className={headerStyles.menuList}>
+            <li className={headerStyles.menuItem}>
+              <a href='#'>Home</a>
+            </li>
+            <li className={headerStyles.menuItem}>
+              <a href='#'>Brows</a>
+            </li>
+            <li className={headerStyles.menuItem}>
+              <a href='#'>Create</a>
+            </li>
+            <li className={headerStyles.menuItem}>
+              <a href='#'>MyNFT's</a>
+            </li>
+            <li>
+              <div className={headerStyles.alarmBox}>
+                <div className={headerStyles.noti} />
+                <IcoAlarm />
+              </div>
+            </li>
+            <li>
+              <div className={headerStyles.accountBox}>
+                <div>Account</div>
+                <div className={headerStyles.profile}>
+                  <img src={profilePic} alt='profile' width={44} height={44} />
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
