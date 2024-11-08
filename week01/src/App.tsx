1cfb78f58c2bb0b1fd8bd4176d162816a16d0e97
dev-hamster <hamsterwave0925@gmail.com>
2024-11-08 22:45:18 +0900

    style: 태블릿 해상도 이상시 화살표 버튼 표시

--- week01/src/App.tsx
+++ week01/src/App.tsx
@@ -1,4 +1,6 @@
 import layout from './app.module.scss';
+import IcoLeft from './assets/ico-left.svg?react';
+import IcoRight from './assets/ico-right.svg?react';
 import Card from './Card';
 import TitleWithButton from './components/TitleWithButton';
 import Header from './Header';
@@ -10,8 +12,27 @@ function App() {
       <Header />
 
       <Hero />
+
       <section className={layout.nftContainer}>
-        <TitleWithButton color='purple' text='Featured NFTs' />
+        <TitleWithButton
+          color='purple'
+          text='Featured NFTs'
+          Button={
+            <>
+              <a href='#' className={layout.seeAllButton}>
+                See All
+              </a>
+              <div className={layout.arrowContainer}>
+                <button className={layout.left}>
+                  <IcoLeft />
+                </button>
+                <button className={layout.right}>
+                  <IcoRight />
+                </button>
+              </div>
+            </>
+          }
+        ></TitleWithButton>
         <Card />
       </section>
     </>
